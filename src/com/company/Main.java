a5fd29c0d00cce3628e7bb0c5b8a04d86b4fa1c6
Dylan LaMott <66742430+GothamsJoker@users.noreply.github.com>
2021-12-03 12:50:04 -0500

    Day Three

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.File;
+import java.util.Scanner;
 // Advent of code 2021
 
 public class Main {
@@ -10,15 +11,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Create lists needed from files
         var something = Util.readFileInt();
         var something2 = Util.readFileString();
-        // Display results to console
-        System.out.println(Util.getSumofIncreasePart1(something));
-        System.out.println(Util.getSumOfIncreasePart2(something));
-        System.out.println(Util.getDirectionPart1(something2));
-        System.out.println(Util.getDirectionPart2(something2));
+
+        boolean validEntry = true;
+
+        while (validEntry) {
+
+            System.out.println("-----Advent of Code 2021!-----");
+            System.out.println("-----User Menu-----");
+            System.out.println("Day 1: 1\nDay 2: 2\nDay 3: 3\nExit: 4\nEnter option: ");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            switch (input) {
+
+                case "1":
+                    System.out.println("Day 1:");
+                    System.out.println(Util.getSumofIncreasePart1(something));
+                    System.out.println(Util.getSumOfIncreasePart2(something));
+                    System.out.println();
+                    break;
+                case "2":
+                    System.out.println("Day 2:");
+                    System.out.println(Util.getDirectionPart1(something2));
+                    System.out.println(Util.getDirectionPart2(something2));
+                    System.out.println();
+                    break;
+                case "3":
+                    System.out.println("Day 3:");
+                    System.out.println(Util.mostCommonBit(something2));
+                    System.out.println(Util.day3Part2(something2));
+                    System.out.println();
+                    break;
+                case "4":
+                    System.out.println("Exiting, Goodbye!");
+                    validEntry = false;
+                default:
+                    System.out.println("Incorrect option try again.");
+                    break;
+            }
+        }
     }
-
-
 }
--- src/com/company/Util.java
+++ src/com/company/Util.java
@@ -5,25 +5,25 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Util {
 
-// Day 1 Part 1
+    // Day 1 Part 1
     public static int getSumofIncreasePart1(List<Integer> y) {
         int change = 0;
         int last = -1;
 
-        for (int x = 0; x < y.size(); x++) {
-            int sum = y.get(x);
+        for (int sum : y) {
             if (last != -1 && last < sum) {
                 ++change;
             }
             last = sum;
         }
-
         return change;
     }
-// Day 1 Part 2
+
+    // Day 1 Part 2
     public static int getSumOfIncreasePart2(List<Integer> y) {
         int increase = 0;
         int count = -1;
@@ -38,46 +38,99 @@ public class Util {
     }
 
     // Day two Part 1
-    public static int getDirectionPart1(List<String> y){
+    public static int getDirectionPart1(List<String> y) {
         int position = 0;
         int depth = 0;
-        for(String string : y){
+        for (String string : y) {
             var split = string.split(" ");
-            if (split[0].equals("down")){
-                depth += Integer.parseInt(split[1]);
-            }else if (split[0].equals("up")){
-                depth -= Integer.parseInt(split[1]);
-            }else if (split[0].equals("forward")){
-                position += Integer.parseInt(split[1]);
+            switch (split[0]) {
+                case "down" -> depth += Integer.parseInt(split[1]);
+                case "up" -> depth -= Integer.parseInt(split[1]);
+                case "forward" -> position += Integer.parseInt(split[1]);
             }
-         }
+        }
         return position * depth;
     }
 
     // Day two part 2
-    public static int getDirectionPart2(List<String> y){
+    public static int getDirectionPart2(List<String> y) {
         int position = 0;
         int depth = 0;
         int aim = 0;
-        for(String string : y){
+        for (String string : y) {
             var split = string.split(" ");
-            if (split[0].equals("down")){
-                aim += Integer.parseInt(split[1]);
-            }else if (split[0].equals("up")){
-                aim -= Integer.parseInt(split[1]);
-            }else if (split[0].equals("forward")){
-                position += Integer.parseInt(split[1]);
-                depth += aim * Integer.parseInt(split[1]);
+            switch (split[0]) {
+                case "down" -> aim += Integer.parseInt(split[1]);
+                case "up" -> aim -= Integer.parseInt(split[1]);
+                case "forward" -> {
+                    position += Integer.parseInt(split[1]);
+                    depth += aim * Integer.parseInt(split[1]);
+                }
             }
         }
-            return position * depth;
+        return position * depth;
     }
 
+    // Day 3 part 1
+    public static int mostCommonBit(List<String> y) {
+        var gamma = new StringBuilder();
+        var ep = new StringBuilder();
 
+        for (int i = 0; i < y.get(0).length(); i++) {
+            int one = 0;
+            int zero = 0;
+
+            for (String string : y) {
+                if (string.charAt(i) == '1') {
+                    ++one;
+                } else {
+                    ++zero;
+                }
+            }
+            if (zero > one) {
+                gamma.append("0");
+                ep.append("1");
+            } else {
+                gamma.append("1");
+                ep.append("0");
+            }
+        }
+        return Integer.parseInt(gamma.toString(), 2) * Integer.parseInt(ep.toString(), 2);
+    }
+
+    // Day 3 part 3
+    public static int day3Part2(List<String> y) {
+        var oxygen = day3Calculation(y, true);
+        var co2 = day3Calculation(y, false);
+
+        return oxygen * co2;
+    }
+
+    public static int day3Calculation(List<String> y, boolean most) {
+        for (int i = 0; y.size() > 1; i++) {
+            int zero = 0;
+            int one = 0;
+
+            for (String string : y) {
+                if (string.charAt(i) == '1') {
+                    ++one;
+                } else {
+                    ++zero;
+                }
+            }
+            final var index = i;
+            var target =
+                    most ? (zero > one ? '0' : '1') : (one < zero ? '1' : '0');
+            y = y.stream().filter(in -> in.charAt(index) == target).collect(Collectors.toList());
+        }
+
+        return Integer.parseInt(y.get(0), 2);
+    }
 
     // Read file and return a list of integers
+    // Enter correct file for testing
     public static List<Integer> readFileInt() {
-
+        // Todo: change to auto place correct file
         File file = new File("src/com/company/instructions/day1/input.txt");
         Scanner scanner = null;
         try {
@@ -86,7 +139,9 @@ public class Util {
             e.printStackTrace();
         }
         List<Integer> integers = new ArrayList<>();
-        while (scanner.hasNext()) {
+        while (true) {
+            assert scanner != null;
+            if (!scanner.hasNext()) break;
             if (scanner.hasNextInt()) {
                 integers.add(scanner.nextInt());
             } else {
@@ -95,10 +150,12 @@ public class Util {
         }
         return integers;
     }
-    // Create a list of string for parsing file
-    public static List<String> readFileString(){
 
-        File file = new File("src/com/company/instructions/day2/input2.txt");
+    // Create a list of string for parsing file
+    // Enter correct file for testing
+    public static List<String> readFileString() {
+        // Todo: change to auto place correct file
+        File file = new File("src/com/company/instructions/day3/input3.txt");
         Scanner scanner = null;
         try {
             scanner = new Scanner(file);
@@ -106,7 +163,9 @@ public class Util {
             e.printStackTrace();
         }
         List<String> strings = new ArrayList<>();
-        while (scanner.hasNext()) {
+        while (true) {
+            assert scanner != null;
+            if (!scanner.hasNext()) break;
             if (scanner.hasNextLine()) {
                 strings.add(scanner.nextLine());
             } else {
@@ -115,5 +174,4 @@ public class Util {
         }
         return strings;
     }
-
 }
